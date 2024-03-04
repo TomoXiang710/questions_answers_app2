d55a7951bbaff0fa829745ce9a0de561bcc44def
tomopu <celticwoman710@gmail.com>
2024-03-05 08:57:07 +0900

    add answer

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,5 @@ class Question < ApplicationRecord
   belongs_to :user
   validates :title, presence: true, length: {maximum: 30}
   validates :content, presence: true
+  has_many :answers, dependent: :destroy
 end
